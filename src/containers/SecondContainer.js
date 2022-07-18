62804c1a0f26bed8265b3ad920de4822d8fc44de
Ali Janbein <alijanbein999@gmail.com>
2022-07-18 13:38:55 +0300

    new profile

--- src/containers/SecondContainer.js
+++ src/containers/SecondContainer.js
@@ -1,8 +1,9 @@
 import classes from "./SecondContainer.module.css";
-import profile from "../images/profile.jpg";
+import profile from "../images/PP-2.png";
 const SecondContainer = () => {
   return (
     <div className={classes.second}>
+    <img className={classes.avatar} src = {profile} alt="Alinj Profile"/>
       <h2>Hello.</h2>
       <p className={classes.intro}>
         im a junior Developer , a web Developer trying to find some expereince.
